8fbf4d6fba89eb4ba5065a8abf8ae351a3433200
Doppins <contact@doppins.com>
2016-04-19 13:27:08 +0000

    Upgrade dependency celery to ==3.1.23

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ tests_require = [
 
 install_requires = [
     'BeautifulSoup>=3.2.1,<3.3.0',
-    'celery>=3.1.8,<3.1.19',
+    'celery==3.1.23',
     'click>=5.0,<7.0',
     'cssutils>=0.9.9,<0.10.0',
     'Django>=1.6.0,<1.7',
